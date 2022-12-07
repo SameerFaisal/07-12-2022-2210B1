2bed33df2369335dc22af0a60e6b0562efbdcc7c
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-07 13:07:56 +0500

    07-12-2022
    Code done in class

---
+++ 07-12-2022/index.js
@@ -0,0 +1,105 @@
+// var vegetable=prompt("Kia laye ho?");
+// if(vegetable==="tomato"){
+//     alert("Tamatar le aya mai")
+// }
+
+// else{
+//     alert("Mai kxh nh laya")
+// }
+
+
+// var num=2
+// if(num===2){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+// }
+
+// var subj1=prompt("Enter marks in subject 1")
+// var subj2=prompt("Enter marks in subject 2")
+// var subj3=prompt("Enter marks in subject 3")
+
+// var obtMarks=parseFloat(subj1)+parseFloat(subj2)+parseFloat(subj3)
+// var percentage=(obtMarks/300)*100
+// alert(percentage.toFixed(2)+"%")
+
+// if(percentage>=90){
+//     alert("Grade is A+")
+// }
+// else if(percentage>=80){
+//     alert("Grade is A")
+// }
+// else if(percentage>=70){
+//     alert("Grade is B")
+// }
+// else if(percentage>=60){
+//     alert("Grade is C")
+// }
+// else{
+//     alert("Sorry you failed")
+// }
+
+
+// alert("Welcome to My Calculator")
+// var number1=prompt("Enter the first number")
+// var number2=prompt("Enter the second number")
+
+// var choice=prompt("Enter + to add, Enter - to subtract, Enter * to multiply, Enter / to divide")
+
+// if(choice==="+"){
+//     var addition=parseFloat(number1)+parseFloat(number2)
+//     document.write("Addition result is: "+addition)
+// }
+// else if(choice==="-"){
+//     var subtract=parseFloat(number1)-parseFloat(number2)
+//     document.write("Subtraction result is: "+subtract)
+// }
+// else if(choice==="*"){
+//     var multiply=parseFloat(number1)*parseFloat(number2)
+//     document.write("Multiplication result is: "+multiply)
+// }
+// else if(choice==="/"){
+//     var divide=parseFloat(number1)/parseFloat(number2)
+//     document.write("Division result is: "+divide)
+// }
+// else{
+//     document.write("Your input is invalid")
+// }
+
+
+// var name=prompt("Enter your name")
+// if(name!=="sameer"){
+//     alert("You cannot login")
+// }
+// else if(name==="sameer"){
+//     alert("Welcome "+name)
+// }
+// else{
+//     alert("Input is invalid")
+// }
+
+// var num1=5
+// var num2=2
+
+// var result=num1%num2
+// alert(result)
+
+// var number=10
+
+// number/=5
+// alert(number)
+
+// var city=prompt("Enter your city")
+// var age=+prompt("Enter your age")
+// if(city==="karachi"&&age>=18){
+//     alert("Congratulations! you are eligible to vote")
+// }
+// else if(city!=="karachi"||age<18){
+// alert("Sorry! You are not eligible to vote")
+// }
+// else{
+//     alert("Input is invalid")
+// }
+
+
